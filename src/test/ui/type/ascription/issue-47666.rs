da8dc6381aa145e9c9ab2b3956203c23328d09b4
Esteban Küber <esteban@kuber.com.ar>
2020-08-09 18:21:03 -0700

    More test cases for enum variant literals typoed as ascription

--- src/test/ui/type/ascription/issue-47666.rs
+++ src/test/ui/type/ascription/issue-47666.rs
@@ -1,4 +1,19 @@
-// run-rustfix
 fn main() {
     let _ = Option:Some(vec![0, 1]); //~ ERROR expected type, found
+}
+fn bar() {
+    let _ = Foo:A; //~ ERROR expected value, found enum `Foo`
+    //~^ ERROR cannot find type `A` in this scope
+    let _ = Foo:B(2); //~ ERROR expected type
+}
+fn baz() {
+    let _ = Foo:C { x: 1 }; //~ ERROR expected one of
+}
+
+enum Foo {
+    A,
+    B(i32),
+    C {
+        x: i32,
+    },
 }
