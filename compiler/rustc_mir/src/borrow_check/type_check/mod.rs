61c2b7aa145b03941e71eecfd341317cef4b6cc2
Esteban Küber <esteban@kuber.com.ar>
2021-06-02 16:16:06 -0700

    Mention where implicit `Sized` obligations come from on E0277
    WIP: ordering of `Sized` obligations can affect the presence of the
    note. Some tweaks needed to make this the best it can be.

--- compiler/rustc_mir/src/borrow_check/type_check/mod.rs
+++ compiler/rustc_mir/src/borrow_check/type_check/mod.rs
@@ -2720,6 +2720,7 @@ impl<'a, 'tcx> TypeChecker<'a, 'tcx> {
             Some(ty::PredicateKind::Trait(
                 ty::TraitPredicate { trait_ref },
                 hir::Constness::NotConst,
+                ty::ImplicitTraitPredicate::No,
             )),
             locations,
             category,
