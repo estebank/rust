d9a7546ed371fd826bc48bd5d6bb5574885b4e61
Esteban Küber <esteban@kuber.com.ar>
2024-12-21 20:25:43 +0000

    Make `Default` const and add some `const Default` impls
    Full list of `impl const Default` types:
    
    - ()
    - bool
    - char
    - std::ascii::Char
    - usize
    - u8
    - u16
    - u32
    - u64
    - u128
    - i8
    - i16
    - i32
    - i64
    - i128
    - f16
    - f32
    - f64
    - f128
    - std::marker::PhantomData<T>
    - Option<T>
    - std::iter::Empty<T>
    - std::ptr::Alignment
    - &[T]
    - &mut [T]
    - &str
    - &mut str
    - String
    - Vec<T>

--- library/core/src/slice/mod.rs
+++ library/core/src/slice/mod.rs
@@ -4942,7 +4942,8 @@ where
 }
 
 #[stable(feature = "rust1", since = "1.0.0")]
-impl<T> Default for &[T] {
+#[rustc_const_unstable(feature = "const_default", issue = "none")]
+impl<T> const Default for &[T] {
     /// Creates an empty slice.
     fn default() -> Self {
         &[]
@@ -4950,7 +4951,8 @@ impl<T> Default for &[T] {
 }
 
 #[stable(feature = "mut_slice_default", since = "1.5.0")]
-impl<T> Default for &mut [T] {
+#[rustc_const_unstable(feature = "const_default", issue = "none")]
+impl<T> const Default for &mut [T] {
     /// Creates a mutable empty slice.
     fn default() -> Self {
         &mut []
