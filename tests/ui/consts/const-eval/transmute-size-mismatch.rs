b4308d1242144c0dd0c8212439dd12053c882b71
Esteban Küber <esteban@kuber.com.ar>
2025-02-19 19:46:34 +0000

    review comment: reword message and add detail to tests

--- tests/ui/consts/const-eval/transmute-size-mismatch.rs
+++ tests/ui/consts/const-eval/transmute-size-mismatch.rs
@@ -10,13 +10,19 @@ const unsafe fn mir_transmute<T, U>(x: T) -> U {
     mir!{
         {
             RET = CastTransmute(x);
+            //~^ NOTE inside `mir_transmute
+            //~| NOTE inside `mir_transmute
+            //~| NOTE the failure ocurred here
+            //~| NOTE the failure ocurred here
             Return()
         }
     }
 }
 
 const FROM_BIGGER: u16 = unsafe { mir_transmute(123_i32) }; //~ ERROR evaluation of constant value failed
+//~^ NOTE transmuting from 4-byte type to 2-byte type: `i32` -> `u16`
 
 const FROM_SMALLER: u32 = unsafe { mir_transmute(123_i16) }; //~ ERROR evaluation of constant value failed
+//~^ NOTE transmuting from 2-byte type to 4-byte type: `i16` -> `u32`
 
 fn main() {}
