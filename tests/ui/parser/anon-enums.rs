68529103ac3f9a3bb4484e656f3ca9b00053096c
Esteban Küber <esteban@kuber.com.ar>
2025-09-24 21:06:54 +0000

    Tweak handling of "struct like start" where a struct isn't supported
    This improves the case where someone tries to write a `match` expr where the patterns have type ascription syntax. Makes them less verbose, by giving up on the first encounter in the block, and makes them more accurate by only treating them as a struct literal if successfuly parsed as such.

--- tests/ui/parser/anon-enums.rs
+++ tests/ui/parser/anon-enums.rs
@@ -4,22 +4,22 @@
 fn foo(x: bool | i32) -> i32 | f64 {
     //~^ ERROR: anonymous enums are not supported
     //~| ERROR: anonymous enums are not supported
-    match x { //~ ERROR: struct literals are not allowed here
+    match x {
         x: i32 => x, //~ ERROR: expected
-        true => 42., //~ ERROR: expected
-        false => 0.333, //~ ERROR: expected
+        true => 42.,
+        false => 0.333,
     }
-} //~ ERROR: expected
+}
 
 fn bar(x: (bool | i32)) -> (i32 | f64) {
     //~^ ERROR: anonymous enums are not supported
     //~| ERROR: anonymous enums are not supported
-    match x { //~ ERROR: struct literals are not allowed here
+    match x {
         x: i32 => x, //~ ERROR: expected
-        true => 42., //~ ERROR: expected
-        false => 0.333, //~ ERROR: expected
+        true => 42.,
+        false => 0.333,
     }
-} //~ ERROR: expected
+}
 
 fn main() {
     match foo(true) {
